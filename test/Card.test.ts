3eb92a8a091ee96927c2e1e2b647e0a8ce01ba18
Gustavo Teixeira <contact@gustavoteixeira.dev>
2022-07-18 23:06:27 -0300

    p1

--- test/Card.test.ts
+++
@@ -1,5 +0,0 @@
-test("Deve criar um cartão", function () {
-    const card = new Card("Atividade 1", 3);
-    expect(card.title).toBe("Atividade 1");
-    expect(card.estimative).toBe(3);
-})
--- test/Column.test.ts
+++
@@ -1,7 +0,0 @@
-import Column from "../src/Column";
-
-test("Deve criar uma coluna", function () {
-    const column = new Column("Coluna A", true);
-    expect(column.name).toBe("Coluna A");
-    expect(column.hasEstimative).toBeTruthy();
-})
